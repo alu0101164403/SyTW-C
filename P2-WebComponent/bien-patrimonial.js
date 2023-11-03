ae8b0686bb1a80f6e0c4ecea34323c945f5bd38f
AinoaIglesias <72820137+alu0101164403@users.noreply.github.com>
2023-11-03 16:57:29 +0000

    Update bien-patrimonial.js

--- P2-WebComponent/bien-patrimonial.js
+++ P2-WebComponent/bien-patrimonial.js
@@ -8,34 +8,23 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     
         connectedCallback() {
-            let bienesAPI = [];
             fetch('https://demo3618184.mockable.io/patrimonio-laLaguna')
                 .then(response => response.json())
                 .then(bienes => {
-                    bienesAPI = bienes;
-                   // bienes.forEach(bien => {
-                        /* const card = document.createElement('bien-patrimonial');
-    
-                        this.shadowRoot.querySelector('#nombre').textContent = bien.nombre;
-                        this.shadowRoot.querySelector('#antecedentes').textContent = bien.antecedentes;
-                        this.shadowRoot.querySelector('#tipo').textContent = bien.tipo;
-                        this.shadowRoot.querySelector('#imagen').src = bien.img;
-                        this.shadowRoot.querySelector('#localizacion').textContent = bien.localizacion;*/
-                        //this.shad owRoot.querySelector('#contenedor-bienes').appendChild(card); // Agrega el elemento a tu contenedor
-                    //});
+                    bienes.bienes.forEach(bien => {
+                        const card = document.createElement('bien-patrimonial');
+                        console.log(card)
+                        card.shadowRoot.querySelector('#nombre').textContent = bien.nombre;
+                        card.shadowRoot.querySelector('#antecedentes').textContent = bien.antecedentes;
+                        card.shadowRoot.querySelector('#tipo').textContent = bien.tipo;
+                        card.shadowRoot.querySelector('#imagen').src = bien.img;
+                        card.shadowRoot.querySelector('#localizacion').textContent = bien.localizacion;
+                        card.shadowRoot.querySelector('#contenedor-bienes').appendChild(card); // Agrega el elemento a tu contenedor
+                    });
                 })
                 .catch(error => {
                     console.error('Error al obtener datos de la API: ', error);
                 });
-            bienesAPI.forEach(bien => {
-                const card = document.createElement('bien-patrimonial');
-                card.shadowRoot.querySelector('#nombre').textContent = bien.nombre;
-                card.shadowRoot.querySelector('#antecedentes').textContent = bien.antecedentes;
-                card.shadowRoot.querySelector('#tipo').textContent = bien.tipo;
-                card.shadowRoot.querySelector('#imagen').src = bien.img;
-                card.shadowRoot.querySelector('#localizacion').textContent = bien.localizacion;
-                card.shadowRoot.querySelector('#contenedor-bienes').appendChild(card); // Agrega el elemento a tu contenedor
-            });
         }
     }
     
